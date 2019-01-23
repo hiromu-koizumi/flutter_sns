d44537e057f8155323fc6726eb408f40bcfc2f7f
hiromu-koizumi <abn21029@gmail.com>
2019-01-23 12:45:58 +0900

    comment&imageurlをfirebaselに保存できる

---
+++ lib/main.dart
@@ -0,0 +1,275 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_cosco/image.dart';
+//import 'package:flutter_cosco/timeline.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
+
+//firebaseに保存されるテキスト。const再代入不可な変数。const変数が指す先のメモリ領域も変更不可
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'cosco',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _FormData {
+  String comment;
+  DateTime date = DateTime.now();
+  String url;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _FormData _data = _FormData();
+
+  @override
+  Widget build(BuildContext context) {
+    DocumentReference _mainReference;
+    _mainReference = Firestore.instance.collection('posts').document();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('投稿'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                print('保存ボタンを押しました');
+              }),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              print('削除ボタンを押しました');
+            },
+          )
+        ],
+      ),
+      body: SafeArea(
+          child: Form(
+            //グローバルキー。紐づけするためにある。
+            key: _formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(20.0),
+              children: <Widget>[
+                //image.dartファイルのクラス
+                addimageButton(),
+                //ImageInput(),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'comment',
+                    labelText: 'コメント',
+                  ),
+
+                  //投稿ボタンが押されたら処理が始まる
+                  onSaved: (String value) {
+                    //valueの中にテキストフィールドに書き込んだ文字が格納されている
+                    _data.comment = value;
+                  },
+
+                  //投稿ボタンが押されたら処理が始まる
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'コメントは必須入力です';
+                    }
+                  },
+
+                  //なんであるかわからないからコメントアウト
+                  //initialValue: _data.comment,
+                ),
+
+                //投稿ボタン
+                RaisedButton(
+                    elevation: 7.0,
+                    child: Text('投稿'),
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: () {
+                      //validatorに処理を送っている
+                      if (_formKey.currentState.validate()) {
+                        //onSavedに処理を送っている
+                        _formKey.currentState.save();
+
+                        //firebaseに写真とテキストを保存する処理
+                        uploadImageText();
+                      }
+
+                      //画面遷移
+                      //Navigator.of(context).pushNamed("/timeline");
+                    })
+              ],
+            ),
+          )),
+    );
+
+  }
+
+
+  //選んだ写真が格納される場所
+  File _imageFile;
+
+  //写真を追加するボタンを押したときの処理
+  void _getImage(BuildContext context, ImageSource source) {
+    //写真の横幅を決めている
+    ImagePicker.pickImage(source: source, maxWidth: 400.0).then((File image) {
+      setState(() {
+
+        //写真を代入
+        _imageFile = image;
+      });
+
+      //他でも使える形式に変更している。
+      // widget.setImage(image);
+      Navigator.pop(context);
+    });
+  }
+
+
+//写真を追加するボタンを押されたとき呼ばれる処理。使う写真を
+  void _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 150.0,
+            padding: EdgeInsets.all(10.0),
+            child: Column(children: [
+              Text(
+                'Pick an Image',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              FlatButton(
+                textColor: Theme.of(context).primaryColor,
+                child: Text('Use Camera'),
+                onPressed: () {
+
+                  //カメラが起動する
+                  _getImage(context, ImageSource.camera);
+                },
+              ),
+              FlatButton(
+                textColor: Theme.of(context).primaryColor,
+                child: Text('Use Gallery'),
+                onPressed: () {
+
+                  //ギャラリーが表示される
+                  _getImage(context, ImageSource.gallery);
+                },
+              )
+            ]),
+          );
+        });
+  }
+
+  Widget addimageButton() {
+    //枠線、アイコン、テキストの色
+    final buttonColor = Theme.of(context).accentColor;
+
+    return Column(
+      children: <Widget>[
+        OutlineButton(
+          //枠線
+          borderSide: BorderSide(
+            color: buttonColor,
+            width: 2.0,
+          ),
+          onPressed: () {
+
+            //写真をギャラリーから選ぶかカメラで今とるかの選択画面を表示
+            _openImagePicker(context);
+          },
+          child: Row(
+            //中心に配置
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+              Icon(
+                Icons.camera_alt,
+                color: buttonColor,
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              Text(
+                'Add Image',
+                style: TextStyle(color: buttonColor),
+              )
+            ],
+          ),
+        ),
+
+        //写真をfirebaseに保存する処理
+        _imageFile == null? Text('写真選択して'):enableUpload(),
+
+      ],
+    );
+  }
+
+  //画像表示する処理。
+  Widget enableUpload(){
+
+    //保存する写真の名前を変更するためにUUIDを生成している
+    final String uuid = Uuid().v1();
+
+    return Container(
+        child: Column(
+            children: <Widget>[
+              Image.file(_imageFile, height: 300.0, width: 300.0),
+//
+            ]
+        )
+    );
+
+  }
+
+  Future<String> uploadImageText() async{
+
+    DocumentReference _mainReference;
+    _mainReference = Firestore.instance.collection('posts').document();
+
+    //_imageFileに格納されている画像をfirebaseStorageに保存している。
+    final StorageReference firebaseStorageRef =
+    //imageフォルダの中に写真を保存している
+    FirebaseStorage.instance.ref().child('image').child('image.jpeg');
+    final StorageUploadTask task = firebaseStorageRef.putFile(_imageFile);
+
+    //写真のurlをダウンロードしている
+    var downUrl = await (await task.onComplete).ref.getDownloadURL();
+
+    //urlに写真のURLを格納
+    _data.url = downUrl.toString();
+
+    print("download url : $_data.url");
+
+    //firebaseDatebaseに保存している
+    _mainReference.setData({
+      "url": _data.url,
+      "comment": _data.comment,
+    });
+
+
+
+
+    return _data.url;
+
+  }
+}
